677ba55cf60a48d9d45db58549d0e238a5d40483
Enrico Olivieri <enrico.olivieri@live.it>
2020-11-05 17:22:19 +0100

    set time in

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ resultEl = document.getElementById('result');
 for (var i = 0; i < 5; i++) {
   arrComp.push(Math.floor(Math.random() * 10));
 }
+
+setTimeout(function () {
+  for (var i = 0; i < 5; i++) {
+    arrUser.push(prompt('Inserisci i numeri in ordine'));
+  }
+}, 30000);
+
+var timer = setInterval(function () {
+  resultEl.innerHTML = --countdown;
+}, 1000);
