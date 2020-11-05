60a6d4a97c21a1da84ac53638f7d952703525c31
Enrico Olivieri <enrico.olivieri@live.it>
2020-11-05 16:59:06 +0100

    starting js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+countdown = 30;
+arrComp = [];
+arrUser = [];
+resultEl = document.getElementById('result');
+
+for (var i = 0; i < 5; i++) {
+  arrComp.push(Math.floor(Math.random() * 10));
+}
